1b57319372bbd715d69654f16d8f237fa9cf4158
David Beltran <85853854+gdbeltran@users.noreply.github.com>
2023-08-26 21:56:25 -0500

    Removed standings page.

--- app/views.py
+++ app/views.py
@@ -1,19 +1,27 @@
-from flask import Flask
 from flask import render_template
 from datetime import datetime
 from . import app
+import statsapi
+import pandas as pd
+from operator import itemgetter
+
+roster_json = statsapi.get('team_roster', {'teamId':142})
+
+roster_table = pd.json_normalize(roster_json['roster'])
+roster = []
+
+for i in range(roster_table.shape[0]):
+    roster.append([int(roster_table.at[i, 'jerseyNumber']), roster_table.at[i, 'person.fullName'], roster_table.at[i, 'position.abbreviation']])
+
+roster.sort(key=itemgetter(0))
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+@app.route("/twinsroster/")
+def twinsroster():
+    return render_template("twinsroster.html",roster=roster)
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
